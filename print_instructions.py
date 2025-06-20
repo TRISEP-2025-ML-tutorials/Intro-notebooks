84f28f7c37df464b3017f865ca3397e53d352210
Wojciech Fedorko <wojtek.fedorko@gmail.com>
2025-06-19 22:29:31 -0700

    add scripts for jupyter

---
+++ print_instructions.py
@@ -0,0 +1,27 @@
+import re
+import os
+
+
+def print_instructions():
+    usr=os.environ['USER']
+        
+    textfile = open('jupyter_logbook.txt', 'r')
+    matches = []
+    reg = re.compile('^\s*http://127.0.0.1:([0-9]*)')
+    print(f"reg: {reg}")
+    for line in textfile:
+        match=reg.match(line)
+        if match is not None:
+            port=match.group(1)
+            print("On your laptop open a new terminal and open an ssh tunnel by pasting this:")
+            print('ssh -L {}:localhost:{} ML1-TRISEP -N -f'.format(port, port))
+            print("then in your browser address bar paste:")
+            print(line.lstrip())
+            
+    textfile.close()
+
+
+
+
+if __name__=='__main__':
+    print_instructions()
